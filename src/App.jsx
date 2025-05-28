68c44d192a619e2c1d0828949cdcc7db4582d63e
CM Mubeen <cmmubeen@gmail.com>
2025-05-28 17:23:12 +0500

    App.jsx is updated by buttons and background color

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ function App() {
 
   return (
     <>
-     <h1>Background Changer App</h1>
+      <div className="h-[100vh] flex flex-col items-center justify-between bg-red-500">
+        <h1 className="text-3xl font-bold mt-6">Background Changer App</h1>
+        <div className="buttons-container mb-10 bg-white py-2 px-4 rounded-lg flex gap-5">
+          <button className="bg-red-500 text-2xl px-8 py-2 rounded-lg">Red</button>
+          <button className="bg-green-500 text-2xl px-8 py-2 rounded-lg">Green</button>
+          <button className="bg-blue-500 text-2xl px-8 py-2 rounded-lg">Blue</button>
+        </div>
+      </div>
     </>
   )
 }
